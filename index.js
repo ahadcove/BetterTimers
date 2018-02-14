9ec895595a179ec05a6d1db27e57fd796a334398
BK Mac <ahadcove@gmail.com>
2018-02-13 22:58:21 -0500

    Added Clear function and types with the Timers

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 module.exports = { Timeout: Timeout, Interval: Interval }
 
+const KEYS = { TIMEOUT: 'timeout', INTERVAL: 'interval'}
+
 function Timeout (fn, interval) {
   var id = setTimeout(fn, interval)
+  this.type = KEYS.TIMEOUT
   this.cleared = false
   this.clear = function () {
     this.cleared = true
@@ -11,9 +14,23 @@ function Timeout (fn, interval) {
 
 function Interval (fn, interval) {
   var id = setInterval(fn, interval)
+  this.type = KEYS.INTERVAL
   this.cleared = false
   this.clear = function () {
     this.cleared = true
     clearInterval(id)
   }
+}
+
+function Clear (timer) {
+    if(timer && timer.id){
+        timer.clear()
+        // if(timer.type === KEYS.TIMEOUT){
+        //     timer.clear()
+        // } else if(timer.type === KEYS.INTERVAL){
+        //     timer.clear()
+        // }
+    } else {
+        return;
+    }
 }
